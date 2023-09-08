dde6319b794ff56afe41bd338a0ee1492a2d7979
lkf6214 <lkf6214@naver.com>
2023-09-08 16:00:11 +0900

    upload

---
+++ src/page/Index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Header from "../component/Header";
+
+export default function Index() {
+  return (
+    <>
+      <div>Index</div>
+      <Header />
+    </>
+  );
+}
